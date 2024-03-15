dbce81ab7ee1653dcad499d54fd52f52cd4440b5
e22206510 <erwan.lebras@etudiant.univ-brest.fr>
2024-03-15 12:07:55 +0100

    first

---
+++ src/main/java/com/example/chatenligne/ChatController.java
@@ -0,0 +1,57 @@
+package com.example.chatenligne;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ChatController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea areaDiscussion;
+
+    @FXML
+    private TextField entreeAdresseIP;
+
+    @FXML
+    private TextField entreeMessage;
+
+    @FXML
+    private TextField entreePort;
+
+    @FXML
+    private TextField entreePseudo;
+
+    @FXML
+    private Label labelEtatConnexion;
+
+    @FXML
+    void actionBoutonConnexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionBoutonDeconnexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionBoutonEnvoyer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
